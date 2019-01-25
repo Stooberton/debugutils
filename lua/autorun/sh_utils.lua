1153a9cd4b526e7ee045631b5a517842eec0dd54
Stephen Jobstein <StevenJobenson@users.noreply.github.com>
2019-01-24 18:30:05 -0600

    Add ShowConstraints

--- lua/autorun/sh_utils.lua
+++ lua/autorun/sh_utils.lua
@@ -15,6 +15,35 @@ if SERVER then
 
 		return Ply:GetEyeTrace()
 	end
+
+	function ShowConstraints()
+		local Ent = Eye().Entity
+
+		if IsValid(Ent) then
+			for _, V in pairs(Ent.Constraints) do
+				local Ent1 = V.Ent1
+				local Ent2 = V.Ent2
+
+				if IsValid(Ent1) and IsValid(Ent2) then
+					local L = V.LPos1 and Ent1:LocalToWorld(V.LPos1) or Ent1:GetPos()
+					local R = V.LPos2 and Ent2:LocalToWorld(V.LPos2) or Ent2:GetPos()
+
+					debugoverlay.Line(L, R, 10, Color(0, 255, 0), true)
+					debugoverlay.Text((L+R)*0.5, V.Type, 10, false)
+				end
+			end
+
+			if IsValid(Ent:GetParent()) then
+				local Parent = Ent:GetParent()
+				
+				local L = Ent:GetPos()
+				local R = Parent:GetPos()
+
+				debugoverlay.Line(L, R, 10, Color(255, 255, 0), true)
+				debugoverlay.Text(L, "Parented", 10, false)
+			end
+		end
+	end
 else
 	function Eye()
 		return LocalPlayer():GetEyeTrace()
